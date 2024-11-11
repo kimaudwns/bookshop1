745fb210657945cd594154d8f1124074c96f1ce4
kimaudwns <red6502001@naver.com>
2024-11-11 13:16:00 +0900

    Update ConstValue.java

--- bookShop01/src/main/java/com/bookshop01/config/ConstValue.java
+++ bookShop01/src/main/java/com/bookshop01/config/ConstValue.java
@@ -4,5 +4,5 @@ public enum ConstValue {
 //  "C:\\shopping\\file_repo"
 //  "/home/ec2-user/shopping/file_repo"
   INSTANCE;
-  public final String DIR = "/home/ec2-user/shopping/file_repo";
+  public final String DIR = "/usr/local/tomcat/webapps/shopping/file_repo";
 }
