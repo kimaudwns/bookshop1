36dd15b86d19af3078d3c23cd2c474cae549cee7
kimaudwns <red6502001@naver.com>
2024-11-11 13:14:52 +0900

    Add files via upload

---
+++ bookShop01/src/main/java/com/bookshop01/config/ConstValue.java
@@ -0,0 +1,8 @@
+package com.bookshop01.config;
+
+public enum ConstValue {
+//  "C:\\shopping\\file_repo"
+//  "/home/ec2-user/shopping/file_repo"
+  INSTANCE;
+  public final String DIR = "/home/ec2-user/shopping/file_repo";
+}
